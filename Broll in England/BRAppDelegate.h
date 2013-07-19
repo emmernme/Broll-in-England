939883a9c04467445a583a4b8694c9fa1351ef94
Emil Broll <Emil@Emil-Brolls-MacBook-Air.local>
2013-07-19 23:00:40 +0200

    Initial Commit

---
+++ Broll in England/BRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRAppDelegate.h
+//  Broll in England
+//
+//  Created by Emil Broll on 19.07.13.
+//  Copyright (c) 2013 Emil Broll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Broll in England/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Broll in England
+//
+//  Created by Emil Broll on 19.07.13.
+//  Copyright (c) 2013 Emil Broll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAppDelegate class]));
+    }
+}
